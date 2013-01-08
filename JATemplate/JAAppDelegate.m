477bd7957f113f30d3f06c1f9a946cd4888d3fee
Jens Ayton <jens@ayton.se>
2013-01-08 23:17:39 +0100

    Improved tests.

--- JATemplate/JAAppDelegate.m
+++ JATemplate/JAAppDelegate.m
@@ -14,10 +14,25 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+	// Test that it works with no parameters.
 	JALog(@"Trivial");
+	
+	// Test substitution.
 	id foo = @"sausage";
 	int bar = 7;
-	JALog(@"foo: {foo}, test 1: {}, test 1B: {(}{)}, test 2: {nonesuch}, bar: {bar}, formatting: {bar|num:spellout}", foo, @( bar ) );
+	JALog(@"foo: {foo}; bar: {bar}", foo, @(bar) );
+	
+	// Test formatting operators. Also a redundant parameter, and reuse of a cached parameter list parse.
+	JALog(@"Fancy bar: {bar|num:spellout}", foo, @(bar) );
+	
+	// Test unknown parameter. Template system should log a warning before this line.
+	JALog(@"Unknown variable: {nonesuch}");
+	
+	// Test brace escapes.
+	JALog(@"Braces: int main() {(} return 0; {)}");
+	
+	
+	[NSApp terminate:nil];
 }
 
 @end
