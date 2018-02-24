bfa3da617b04a5a5e3ffbc875f23bd2ea0f92fd7
Jens Ayton <jens@ayton.se>
2018-02-24 13:29:26 +0100

    Fix failure when processing strings greater than 1024 characters

--- JATemplate/JATemplateCore.m
+++ JATemplate/JATemplateCore.m
@@ -517,7 +517,7 @@ void JATWithCharacters(NSString *string, void(^block)(const unichar characters[]
 	{
 		// Otherwise, we need to do it on the heap.
 		mutableStringBuffer = malloc(sizeof *stringBuffer * length);
-		if (stringBuffer == NULL)  return;
+		if (mutableStringBuffer == NULL)  return;
 	}
 	else
 	{
